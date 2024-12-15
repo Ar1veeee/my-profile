912ed8f1530ffbff1dfe6b7db8b0bbc665bd825f
Alief Arifin M <aliefarifin99@gmail.com>
2024-12-15 11:32:30 +0700

    adding dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,19 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle('hidden');
 });
+
+const checkbox = document.querySelector('#toggle');
+const html = document.querySelector('html');
+const header = document.querySelector('header');
+
+checkbox.addEventListener('click', function () {
+  if (checkbox.checked) {
+    html.classList.add('dark');
+    header.classList.remove('navbar-fixed');
+    header.classList.add('navbar-fixed-dark');
+  } else {
+    html.classList.remove('dark');
+    header.classList.remove('navbar-fixed-dark');
+    header.classList.add('navbar-fixed');
+  }
+});
